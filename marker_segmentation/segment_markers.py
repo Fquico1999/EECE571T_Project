8bfabd63be717c894b6bddc9de6b1ed58ac481e3
Francisco Farinha <fquico1999@gmail.com>
2020-04-28 19:54:18 -0700

    Scripts for training and producing labelled segmentations

---
+++ marker_segmentation/segment_markers.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+import os
+import numpy as np
+import cv2
+from tensorflow.keras import models
+import argparse
+from tqdm import tqdm
+import json
+
+if __name__ == '__main__':
+
+    #Read configuration file
+    with open('config.json') as f:
+        config = json.load(f)
+
+    IMG_W = config['image_width']
+    IMG_H = config['image_height']
+
+    win_w = config['window_width']
+    win_h = config['window_height']
+
+    scaled_win_w = config['model_input_width']
+    scaled_win_h = config['model_input_height']
+
+    padding = config['padding']
+
+    padless_win_w = scaled_win_w-2*padding
+    padless_win_h = scaled_win_h-2*padding
+
+    num_win_x = IMG_W/win_w
+    num_win_y = IMG_H/win_h
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-i','--images', type=str, required=True, help='path to image dataset')
+    ap.add_argument('-mp', '--model_path', type=str, required=True, help='path of model')
+    ap.add_argument('-o', '--output_folder', type=str, required=False, default='./markers', help='Path to filtered image output folder')
+    ap.add_argument('-c','--color', type=list, required=False, default=[0,0,255], help='Marker label color in BGR ([B,G,R])')
+    ap.add_argument('-m','--output_masks',type=bool, required=False, default=False, help='Whether to also output mask')
+    args = vars(ap.parse_args())
+
+    try:
+        model = models.load_model(args['model_path'], compile=False) 
+        model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=dice_loss, metrics=[dice_loss,'accuracy'])
+
+    except Exception as e:
+        print(e)
+
+    img_names = os.listdir(args['images'])
+    
+
+    #Data Collection
+    for img_name in tqdm(img_names, desc='Segmenting Markers'):
+        X = []
+
+        #Obtain image path
+        img_path = os.path.join(args['images'], img_name)
+
+        #Read image
+        img = cv2.imread(img_path)
+
+        #Ensure image size is IMG_WxIMG_H
+        if img.shape[0:2] != (IMG_W, IMG_H):
+            img = cv2.resize(img, (IMG_W, IMG_H))
+
+        #Create image patches
+        cols = np.arange(0, num_win_x*win_w, win_w)
+        rows = np.arange(0, num_win_y*win_h, win_h)
+        for row in rows:
+            for col in cols:
+                win_img = img[col:col+win_w,row:row+win_w,:]/255.0
+
+                padless_win_img = cv2.resize(win_img, (padless_win_w, padless_win_h))
+                scaled_win_img = cv2.copyMakeBorder( padless_win_img, padding, padding, padding, padding, cv2.BORDER_REPLICATE)
+        
+                X.append(scaled_win_img)
+    
+        X = np.asarray(X)
+
+        #Get y patches
+        y = model.predict(X)
+
+        #Create mask
+        mask = np.zeros((num_win_x*padless_win_w, num_win_y*padless_win_h))
+
+        cols = np.arange(0, num_win_x*padless_win_w, padless_win_w)
+        rows = np.arange(0, num_win_y*padless_win_h, padless_win_h)
+
+        for i in range(len(rows)):
+            for j in range(len(cols)):
+                patch = y[(i*num_win_x)+j][padding:-padding,padding:-padding] #remove padding
+                patch = np.reshape(patch, (padless_win_w, padless_win_h)) #remove last dimension
+
+                mask[cols[j]:cols[j]+padless_win_h,rows[i]:rows[i]+padless_win_w] = patch*255
+
+        resized_mask = cv2.resize(mask, (IMG_W, IMG_H))
+
+        int_mask = np.uint8(resized_mask)
+
+        #ret, thresh = cv2.threshold(int_mask, 0.1*np.max(int_mask), 255, cv2.THRESH_BINARY)
+        ret, thresh = cv2.threshold(int_mask, 3*np.std(int_mask), 255, cv2.THRESH_BINARY)
+
+        nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity=8)
+        sizes = stats[1:, -1]; nb_components = nb_components - 1
+
+        min_size = 1
+
+        mask_proc = np.zeros(resized_mask.shape)
+        #for every component in the image, you keep it only if it's above min_size
+        for i in range(0, nb_components):
+            if sizes[i] >= min_size:
+                mask_proc[output == i + 1] = 255
+
+        #Close gaps in mask and dilate
+        kernel_c = np.ones((2,2),np.uint8)
+        kernel_d = np.ones((5,5),np.uint8)
+
+        mask_proc = cv2.morphologyEx(mask_proc, cv2.MORPH_CLOSE, kernel_c)
+        mask_proc = cv2.dilate(mask_proc,kernel_d,iterations = 1)
+
+        img_o = img.copy()
+        idx = np.where(mask_proc == 255)
+        img_o[idx] = args['color']
+
+        if args['output_masks']:
+            output_path = os.path.join(args['output_folder'],img_name.split('.')[0] + '_mask.jpg')
+            cv2.imwrite(output_path, mask_proc)
+
+        output_path = os.path.join(args['output_folder'],img_name.split('.')[0] + '_labelled.jpg')
+        cv2.imwrite(output_path, img_o)
--- marker_segmentation/train_unet.py
+++ marker_segmentation/train_unet.py
@@ -39,10 +39,11 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-i','--images', type=str, required=True, help='path to image dataset')
     ap.add_argument('-m', '--masks', type=str, required=True, help='path to mask dataset')
-    ap.add_argument('-e', '--epochs', type=int, required=False, default=3, help='number of epochs to train on')
+    ap.add_argument('-e', '--epochs', type=int, required=False, default=35, help='number of epochs to train on')
     ap.add_argument('-b', '--num_batches', type=int, required=False, default=16, help='number of batches to train on')
     ap.add_argument('-mp', '--model_path', type=str, required=False, default=None, help='path of model to train')
     ap.add_argument('-vs', '--validation_split', type=float, required=False, default=0.15, help='Training validation_split')
+    ap.add_argument('-ri', '--rename_images', type=bool, required=False, default=False, help='Whether to rename images to lowercase for mask match')
     args = vars(ap.parse_args())
 
     img_names = os.listdir(args['images'])
@@ -51,13 +52,22 @@ if __name__ == '__main__':
     X = []
     y = []
 
+    #Because of annotation software, there may be a mismatch in naming
+    if args['rename_images']:
+        for img_name in img_names:
+            img_path_curr = os.path.join(img_path, img_name)
+            img_path_lower = os.path.join(img_path, img_name.lower())
+            os.rename(img_path_curr, img_path_lower)
+        #After renaming, get img_names again
+        img_names = os.listdir(args['images'])
+
     #Data Collection
     for i in tqdm(range(len(img_names)), desc='Collecting Data'):
 
         #Obtain mask and image paths
         mask_path = os.path.join(args['masks'], mask_names[i])
         img_name = mask_names[i].split('_mask')[0]+'.jpg'
-        assert img_name in img_names, 'Expected {} in {}'.format(img_name, args['images'])
+        assert img_name in img_names, 'Expected {} in {}, if image filename is uppercase, use --ri = True'.format(img_name, args['images'])
         img_path = os.path.join(args['images'], img_name)
 
         #Read mask and img in
@@ -66,10 +76,6 @@ if __name__ == '__main__':
         img = cv2.imread(img_path)
         scaled_img = cv2.resize(img, (scaled_win_w, scaled_win_h))/255.0
         scaled_mask = cv2.resize(thresh, (scaled_win_w, scaled_win_h))/255.0
-        cv2.imshow('img',scaled_img)
-        cv2.waitKey(1)
-        cv2.imshow('mask', scaled_mask)
-        cv2.waitKey(0)
 
         X.append(scaled_img)
         y.append(scaled_mask)
@@ -94,45 +100,23 @@ if __name__ == '__main__':
         c2 = layers.Dropout(0.1)(c2)
         c2 = layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
 
-        p2 = layers.MaxPooling2D((2,2))(c2)
-        c3 = layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
-        c3 = layers.Dropout(0.2)(c3)
-        c3 = layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+        u1 = layers.Conv2DTranspose(16, (2,2), strides=(2,2), padding='same')(c2)
+        u1 = layers.concatenate([u1,c1])
+        c3 = layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u1)
+        c3 = layers.Dropout(0.1)(c3)
+        c3 = layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
 
-        p3 = layers.MaxPooling2D((2,2))(c3)
-        c4 = layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
-        c4 = layers.Dropout(0.2)(c4)
-        c4 = layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
-
-        u5 = layers.Conv2DTranspose(64, (2,2), strides=(2,2), padding='same')(c4)
-        u5 = layers.concatenate([u5,c3])
-        c5 = layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u5)
-        c5 = layers.Dropout(0.2)(c5)
-        c5 = layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
-
-        u6 = layers.Conv2DTranspose(32, (2,2), strides=(2,2), padding='same')(c5)
-        u6 = layers.concatenate([u6,c2])
-        c6 = layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
-        c6 = layers.Dropout(0.1)(c6)
-        c6 = layers.Conv2D(32, (3,3), activation='linear', kernel_initializer='he_normal', padding='same')(c6)
-
-        u7 = layers.Conv2DTranspose(16, (2,2), strides=(2,2), padding='same')(c6)
-        u7 = layers.concatenate([u7,c1])
-        c7 = layers.Conv2D(16, (3,3), activation='linear', kernel_initializer='he_normal', padding='same')(u7)
-        c7 = layers.Dropout(0.1)(c7)
-        c7 = layers.Conv2D(16, (3,3), activation='linear', kernel_initializer='he_normal', padding='same')(c7)
-
-        outputs = layers.Conv2D(1,(1,1), activation='sigmoid')(c7)
+        outputs = layers.Conv2D(1,(1,1), activation='sigmoid')(c3)
 
         model = Model(inputs=[inputs], outputs=[outputs])
-        model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=dice_loss, metrics=['accuracy'])
+        model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=dice_loss, metrics=[dice_loss,'accuracy'])
 
     else:
         print('Model path specified, loading model')
 
         try:
             model = models.load_model(args['model_path'], compile=False) 
-            model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=dice_loss, metrics=['accuracy'])
+            model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss=dice_loss, metrics=[dice_loss,'accuracy'])
 
         except Exception as e:
             print(e)
@@ -154,6 +138,8 @@ if __name__ == '__main__':
     fig.set_size_inches(12,8)
     ax.plot(history.history['acc'], label='accuracy')
     ax.plot(history.history['val_acc'], label='validation_accuracy')
+    ax.plot(history.history['dice_loss'], label='dice_loss')
+    ax.plot(history.history['val_dice_loss'], label='validation_dice_loss')
     ax.set_xlabel('Epoch')
     plt.legend()
     plt.savefig('history.png')
